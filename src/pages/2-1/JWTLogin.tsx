57b3ca77c4c9a98b73ae1f5fe530d7a0e3cab308
조혜연 <hy_312@mncnext.co.kr>
2023-03-10 22:47:33 +0900

    Modify: 로그인 수정

--- src/pages/2-1/JWTLogin.tsx
+++ src/pages/2-1/JWTLogin.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { getCurrentUserInfoWithToken, loginWithToken } from '../../api/login'
 import { UserInfo } from '../../types/user'
-import axios from "axios";
 
 const JWTLogin = () => {
   const [userInfo, setUserInfo] = useState<UserInfo | null>(null)
@@ -11,35 +10,28 @@ const JWTLogin = () => {
 
     const formData = new FormData(event.currentTarget)
 
-    const loginPayload = {
+    const loginResult = await loginWithToken({
       username: formData.get('username') as string,
       password: formData.get('password') as string
-    }
-    console.log('loginPayload',loginPayload)
-
+    })
     // TODO: 로그인 연결 및 토큰 가져오기 (loginWithToken 함수 사용)
     // 로그인 실패시 함수를 종료합니다.
     // 로그인 성공시, getCurrentUserInfoWithToken 함수를 호출하여 userInfo를 가져옵니다.
+    if (loginResult.result === 'fail') return
 
-    axios.post('http://wanted-p2.bluestragglr.com/auth/login', {
-      username: `${loginPayload.username}`,
-      password: `${loginPayload.password}`,
-    })
-        .then(function (response) {
-          console.log(response);
-          getCurrentUserInfoWithToken(response.data.access_token);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
     // TODO: 유저 정보 가져오기 (getCurrentUserInfoWithToken 함수 사용)
     // 유저 정보 가져오기 실패시 함수를 종료합니다.
     // 유저 정보 가져오기 성공시, userInfo 상태를 업데이트합니다.
+    const userInfo = await getCurrentUserInfoWithToken(loginResult.access_token)
+
+    if (userInfo === null) return
+
+    setUserInfo(userInfo)
   }
 
   return (<div>
     <h1>
-      Login with JWT - in memory
+      Login with Mock API
     </h1>
     <form onSubmit={loginSubmitHandler}>
       <label>
